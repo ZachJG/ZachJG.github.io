1ff7500c5124adb210807d9fed7d7686f679da0c
ZachJG <z_goloyogo@u.pacific.edu>
2026-01-21 13:17:49 -0800

    added triangles

---
+++ shapes/triangle1.js
@@ -0,0 +1,14 @@
+import Standard2DVertexObject from "../classes/vertexObject2d";
+
+export default class Triangle1 extends Standard2DVertexObject {
+    constructor(device, canvasFormat) {
+        let vertices = new Float32Array([
+            // x, y
+            0, 0.5,
+            -0.5, 0,
+            0.5,  0
+        ]);
+        super(device, canvasFormat, vertices, '/shaders/standard2d.wgsl', 'triangle-list');
+        this._vertices = vertices;
+    }
+}
