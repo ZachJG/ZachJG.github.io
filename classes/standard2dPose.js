c56d97ba62dfd4bc2d526d06deeda68e5a16d547
ZachJG <z_goloyogo@u.pacific.edu>
2026-01-28 10:20:28 -0800

    ga poser

---
+++ classes/standard2dPose.js
@@ -0,0 +1,43 @@
+import Standard2DVertexObject from "./vertexObject2d";
+
+export default class Standard2DGAPosedVertexObject extends Standard2DVertexObject {
+    constructor(device, canvasFormat, vertices, pose, shaderFile, topology) {
+        super(device, canvasFormat, vertices, shaderFile, topology);
+        this._pose = pose;
+    }
+
+    // More methods to implement
+    async createGeometry() {
+        // Call parent's to create vertex geometry
+        super.createGeometry();
+        // Create pose buffer to store the object pose in GPU
+        this._poseBuffer = this._device.createBuffer({
+            label: "Pose " + this.getName(),
+            size: this._pose.byteLength,
+            usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+        });
+        // Initial update of the geomtery - in this case only the pose
+        this.updateGeometry();
+    }
+    updateGeometry() {
+        // Copy from pose from CPU to GPU
+        this._device.queue.writeBuffer(this._poseBuffer, 0, this._pose);
+    }
+    async createRenderPipeline() {
+        // Call parent's to create render pipeline
+        super.createRenderPipeline();
+        // Creata a bind group to pass the pose buffer into @group(0) @binding(0)
+        this._bindGroup = this._device.createBindGroup({
+            label: "Render Bind Group " + this.getName(),
+            layout: this._renderPipeline.getBindGroupLayout(0),
+            entries: [
+                {binding: 0,
+                resource: { buffer: this._poseBuffer },}
+            ],
+        });
+    }
+    render(pass) {
+        pass.setBindGroup(0, this._bindGroup);  // bind the pose buffer
+        super.render(pass);                     // reuse the render function
+    }
+}
--- spell 3/spell3.js
+++ spell 3/spell3.js
@@ -1,6 +1,7 @@
 import Standard2DFullScreenObject from "../classes/standard2dFullScreenObject.js";
 import ImageFilterObject from "../classes/imageFilterObject.js";
 import ImageNosifyFilterObject from "../classes/imageNosify.js";
+import Standard2DGAPosedVertexObject from "../classes/standard2dPose.js";
 
 let shaderCode = await this.loadShader("../shaders/fullScreenTexture.wgsl");
 
@@ -9,3 +10,24 @@ await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device
 await renderer.appendFilterObject(new ImageFilterObject(renderer._device, renderer._canvasFormat, "../shaders/8bit.wgsl"));
 
 await renderer.appendFilterObject(new ImageNosifyFilterObject(renderer._device, renderer._canvasFormat, "../shaders/nosify.wgsl"));
+
+let applyRotorToRotor = (dr, r) => {
+    // r = cS + s exey
+    // dr = ccS + ss exey
+    // dr r = (c * cc - s * ss)S + (cc * s + ss * s) e01
+    return [dr[0] * r[0] - dr[1] * r[1], dr[0] * r[1] + dr[1] * r[0]];
+};
+
+var pose = [1, 0, 0, 0, 1, 1]; // rotor, translator, scales
+pose = new Float32Array(pose);
+await renderer.appendSceneObject(new Standard2DGAPosedVertexObject(renderer._device, renderer._canvasFormat, vertices, pose, "../shaders/gaposer.wgsl", "triangle-list"));
+let angle = Math.PI / 100 / 2;
+let dr = [Math.cos(angle), -Math.sin(angle)]; // a delta rotor
+
+setInterval(() => { 
+    renderer.render();
+    // update pose
+    let newrotor = applyRotorToRotor(dr, [pose[0], pose[1]]);
+    pose[0] = newrotor[0];
+    pose[1] = newrotor[1];
+}, 100); // call every 100 ms
