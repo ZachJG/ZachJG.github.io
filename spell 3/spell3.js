34ff898f3a041580cdb2e40104a22510c27f5799
ZachJG <z_goloyogo@u.pacific.edu>
2026-01-28 10:03:55 -0800

    added nosify

--- spell 3/spell3.js
+++ spell 3/spell3.js
@@ -1,8 +1,11 @@
 import Standard2DFullScreenObject from "../classes/standard2dFullScreenObject.js";
 import ImageFilterObject from "../classes/imageFilterObject.js";
+import ImageNosifyFilterObject from "../classes/imageNosify.js";
 
 let shaderCode = await this.loadShader("../shaders/fullScreenTexture.wgsl");
 
 await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device, renderer._canvasFormat, "../assets/boatgoesbinted.jpg"));
 
 await renderer.appendFilterObject(new ImageFilterObject(renderer._device, renderer._canvasFormat, "../shaders/8bit.wgsl"));
+
+await renderer.appendFilterObject(new ImageNosifyFilterObject(renderer._device, renderer._canvasFormat, "../shaders/nosify.wgsl"));
