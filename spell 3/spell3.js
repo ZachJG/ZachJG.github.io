db1ec71af1ff6e74502fbd7cfabeb2b187b08a86
ZachJG <z_goloyogo@u.pacific.edu>
2026-01-28 09:50:30 -0800

    boatgoesbinted

--- spell 3/spell3.js
+++ spell 3/spell3.js
@@ -1,5 +1,8 @@
 import Standard2DFullScreenObject from "../classes/standard2dFullScreenObject.js";
+import ImageFilterObject from "../classes/imageFilterObject.js";
 
 let shaderCode = await this.loadShader("../shaders/fullScreenTexture.wgsl");
 
-await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device, renderer._canvasFormat, "../assets/<an image file>"));
+await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device, renderer._canvasFormat, "../assets/boatgoesbinted.jpg"));
+
+ await renderer.appendFilterObject(new ImageFilterObject(renderer._device, renderer._canvasFormat, "../shaders/dummyShader.wgsl"));
