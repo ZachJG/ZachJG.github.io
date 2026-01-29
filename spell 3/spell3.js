85cb7a35d5150b2263442ea917f6a6500911c7fe
ZachJG <z_goloyogo@u.pacific.edu>
2026-01-28 16:07:54 -0800

    changed spell3

--- spell 3/spell3.js
+++ spell 3/spell3.js
@@ -1,5 +1,3 @@
 import Standard2DFullScreenObject from "../classes/standard2dFullScreenObject.js";
 
-let shaderCode = await this.loadShader("../shaders/fullScreenTexture.wgsl");
-
 await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device, renderer._canvasFormat, "../assets/boatgoesbinted.jpg"));
