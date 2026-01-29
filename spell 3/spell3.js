e1fe073b7db59ed90c9f5c43fe88501b35b74eca
ZachJG <z_goloyogo@u.pacific.edu>
2026-01-29 09:53:06 -0800

    changed spell 3

--- spell 3/spell3.js
+++ spell 3/spell3.js
@@ -1,3 +1,29 @@
+import Renderer from "../classes/renderer2d.js";
 import Standard2DFullScreenObject from "../classes/standard2dFullScreenObject.js";
 
-await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device, renderer._canvasFormat, "../assets/boatgoesbinted.jpg"));
+
+async function init() {
+  // Create a canvas tag
+  const canvasTag = document.createElement('canvas');
+  canvasTag.id = "renderCanvas";
+  document.body.appendChild(canvasTag);
+
+  // Create a simple renderer
+  const renderer = new Renderer(canvasTag);
+  await renderer.init();
+
+  
+
+  // Render
+  renderer.render();
+  return renderer;
+}
+
+init().then( ret => {
+  console.log(ret);
+}).catch( error => {
+  const pTag = document.createElement('p');
+  pTag.innerHTML = navigator.userAgent + "</br>" + error.message;
+  document.body.appendChild(pTag);
+  document.getElementById("renderCanvas").remove();
+});
