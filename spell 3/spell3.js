af615f43d56a9dbcfb416c3934ef350d40bda72a
ZachJG <z_goloyogo@u.pacific.edu>
2026-02-04 09:48:03 -0800

    updated spell

--- spell 3/spell3.js
+++ spell 3/spell3.js
@@ -4,6 +4,12 @@ import ImageNosifyFilterObject from "../classes/imageNosify.js";
 import Standard2DGAPosedVertexObject from "../classes/standard2dPose.js"
 
 async function init() {
+  let applyRotorToRotor = (dr, r) => {
+    // r = cS + s exey
+    // dr = ccS + ss exey
+    // dr r = (c * cc - s * ss)S + (cc * s + ss * s) e01
+    return [dr[0] * r[0] - dr[1] * r[1], dr[0] * r[1] + dr[1] * r[0]];
+  };
   // Create a canvas tag
   const canvasTag = document.createElement('canvas');
   canvasTag.id = "renderCanvas";
@@ -30,9 +36,16 @@ async function init() {
     0, 0.5    // r_center
   ]);
   await renderer.appendSceneObject(new Standard2DGAPosedVertexObject(renderer._device, renderer._canvasFormat, vertices, pose, "../shaders/gaposer.wgsl", "triangle-list"));
-
+  let angle = Math.PI / 100 / 2;
+  let dr = [Math.cos(angle), -Math.sin(angle)]; // a delta rotor
   // Render
-  renderer.render();
+  setInterval(() => { 
+    renderer.render();
+    // update pose
+    let newrotor = applyRotorToRotor(dr, [pose[0], pose[1]]);
+    pose[0] = newrotor[0];
+    pose[1] = newrotor[1];
+  }, 100); // call every 100 ms
   return renderer;
 }
 
