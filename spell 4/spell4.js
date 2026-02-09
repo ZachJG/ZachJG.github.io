e35e5689ea83ce991dd2886d73218f1c068ffbfe
ZachJG <z_goloyogo@u.pacific.edu>
2026-02-09 10:40:41 -0800

    removed triangle

--- spell 4/spell4.js
+++ spell 4/spell4.js
@@ -2,7 +2,6 @@ import FilteredRenderer from "../classes/filteredRenderer.js";
 import Standard2DFullScreenObject from "../classes/standard2dFullScreenObject.js";
 import Camera from "../classes/camera.js";
 import Camera2DVertexObject from "../classes/cameraVertexObject.js";
-import Triangle1 from "../shapes/triangle1.js";
 
 async function init() {
     // Create a canvas tag
@@ -15,8 +14,6 @@ async function init() {
     await renderer.init();
 
     await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device, renderer._canvasFormat, "../assets/boatgoesbinted.jpg"));
-    await renderer.appendSceneObject(new Triangle1(renderer._device, renderer._canvasFormat));
-
     let camera = new Camera();
     let triangle = new Camera2DVertexObject(renderer._device, renderer._canvasFormat, camera._pose, new Float32Array([0, 0.5, -0.5, 0, 0.5, 0]), "../shaders/camera.wgsl");
     await renderer.appendSceneObject(triangle);
