68b7c9680e80c1fbb4b41836f39f3eae97cd2580
ZachJG <z_goloyogo@u.pacific.edu>
2026-02-11 14:27:49 -0800

    text object

---
+++ spell 5/spell5.js
@@ -0,0 +1,23 @@
+import Renderer from "../classes/renderer2d.js";
+
+async function init() {
+    // Create a canvas tag
+    const canvasTag = document.createElement('canvas');
+    canvasTag.id = "renderCanvas";
+    document.body.appendChild(canvasTag);
+
+    // Create a simple renderer
+    const renderer = new Renderer(canvasTag);
+    await renderer.init();
+
+    return renderer;
+}
+
+init().then( ret => {
+  console.log(ret);
+}).catch( error => {
+  const pTag = document.createElement('p');
+  pTag.innerHTML = navigator.userAgent + "</br>" + error.message;
+  document.body.appendChild(pTag);
+  document.getElementById("renderCanvas").remove();
+});
