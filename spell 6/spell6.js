135fca381ab703d7e70b39c59cebc6672b207227
ZachJG <z_goloyogo@u.pacific.edu>
2026-02-18 17:37:34 -0800

    spell6

--- spell 6/spell6.js
+++ spell 6/spell6.js
@@ -1,6 +1,7 @@
 import FilteredRender from "../classes/filteredRenderer.js";
 import Camera from "../classes/camera.js";
 import ParticleSystemObject from "../classes/particle.js";
+import StandardTextObject from "../classes/textObject.js";
 
 async function init() {
     // Create a canvas tag
@@ -11,8 +12,38 @@ async function init() {
     // Create a simple renderer
     const renderer = new FilteredRender(canvasTag);
     await renderer.init();
+    
+    let particles = new ParticleSystemObject(renderer._device,renderer._canvasFormat,"../shaders/particles.wgsl",128);
+    await renderer.appendSceneObject(particles);
 
     let camera = new Camera();
+    let fps = '??';
+    var fpsText = new StandardTextObject('fps: ' + fps);
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case 'f': case 'F': fpsText.toggleVisibility(); break;
+      }
+    });
+    var frameCnt = 0
+    var tgtFPS = 60;
+    var secPerFrame = 1. / tgtFPS;
+    var frameInterval = secPerFrame * 1000;
+    var lastCalled;
+    let renderFrame = () => {
+      let elapsed = Date.now() - lastCalled;
+      if (elapsed > frameInterval) {
+        ++frameCnt;
+        lastCalled = Date.now() - (elapsed % frameInterval);
+        renderer.render();
+      }
+      requestAnimationFrame(renderFrame);
+    };
+    lastCalled = Date.now();
+    renderFrame();
+    setInterval(() => { 
+      fpsText.updateText('fps: ' + frameCnt);
+      frameCnt = 0;
+    }, 1000); // call every 1000 ms
     return renderer;
 }
 
