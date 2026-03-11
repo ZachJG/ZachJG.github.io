3c71ebfd034209bdd9b0911b1f6255e28fc4d46e
ZachJG <z_goloyogo@u.pacific.edu>
2026-03-10 17:55:52 -0700

    quest 1

---
+++ quests/quest1.js
@@ -0,0 +1,31 @@
+import Renderer from "../classes/renderer2d.js";
+import SmallTriangle from "../shapes/smallTriangle.js";
+import Square from "../shapes/square.js";
+
+async function init() {
+  // Create a canvas tag
+  const canvasTag = document.createElement('canvas');
+  canvasTag.id = "renderCanvas";
+  document.body.appendChild(canvasTag);
+
+  // Create a simple renderer
+  const renderer = new Renderer(canvasTag);
+  await renderer.init();
+
+  // Append objects
+  await renderer.appendSceneObject(new SmallTriangle(renderer._device, renderer._canvasFormat));
+  await renderer.appendSceneObject(new Square(renderer._device, renderer._canvasFormat));
+
+  // Render
+  renderer.render();
+  return renderer;
+}
+
+init().then( ret => {
+  console.log(ret);
+}).catch( error => {
+  const pTag = document.createElement('p');
+  pTag.innerHTML = navigator.userAgent + "</br>" + error.message;
+  document.body.appendChild(pTag);
+  document.getElementById("renderCanvas").remove();
+});
