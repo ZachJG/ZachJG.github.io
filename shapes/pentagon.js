342ecb28b87c700ae35b4348b460fe57f39d74ff
ZachJG <z_goloyogo@u.pacific.edu>
2026-03-10 17:46:42 -0700

    added pentagon

---
+++ shapes/pentagon.js
@@ -0,0 +1,15 @@
+import Standard2DVertexObject from "../classes/vertexObject2d.js";
+
+export default class SmallTriangle extends Standard2DVertexObject {
+    constructor(device, canvasFormat) {
+        let vertices = new Float32Array([
+            // x, y
+            0.1, 0.1,
+            0.05, 0.1,
+            0.1,  0.0,
+            0.05, 0.0
+        ]);
+        super(device, canvasFormat, vertices, '../shaders/standard2d.wgsl', 'triangle-list');
+        this._vertices = vertices;
+    }
+}
