bf262679533b7284b03758c392626fb176344bbc
ZachJG <z_goloyogo@u.pacific.edu>
2026-03-10 17:12:46 -0700

    quest 1

---
+++ shapes/smallTriangle.js
@@ -0,0 +1,14 @@
+import Standard2DVertexObject from "../classes/vertexObject2d.js";
+
+export default class SmallTriangle extends Standard2DVertexObject {
+    constructor(device, canvasFormat) {
+        let vertices = new Float32Array([
+            // x, y
+            -0.8, -0.6,
+            -0.6, -0.8,
+            -0.4,  -0.6
+        ]);
+        super(device, canvasFormat, vertices, '../shaders/standard2d.wgsl', 'triangle-list');
+        this._vertices = vertices;
+    }
+}
